e6662efb181656ab0633bd560e5f783ac8d8fd25
Fedrianto R <madindo@gmail.com>
2020-12-11 10:17:30 +0700

    update

--- controllers/homecontroller.go
+++ controllers/homecontroller.go
@@ -7,3 +7,7 @@ import (
 func HomeIndex(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "HomeResponse")
 }
+
+func HomeLoaderIo(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "HomeResponseloaderio-e8b5a8357a3427f049705ec89394e548")
+}
--- router/web.go
+++ router/web.go
@@ -4,18 +4,19 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	maincontroller "github.com/madindo/harukaedu-main/controllers"
+	homecontroller "github.com/madindo/harukaedu-main/controllers"
 	usercontroller "github.com/madindo/harukaedu-users/controllers"
 )
 
 func Web() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", maincontroller.HomeIndex ).Methods("GET")
+	myRouter.HandleFunc("/", homecontroller.HomeIndex ).Methods("GET")
 
 	myRouter.HandleFunc("/users", usercontroller.UserIndex).Methods("GET")
 	myRouter.HandleFunc("/users", usercontroller.UserStore).Methods("POST")
 	myRouter.HandleFunc("/users/{id}", usercontroller.UserUpdate).Methods("PUT")
 	myRouter.HandleFunc("/users/{id}", usercontroller.UserDelete).Methods("DELETE")
+	myRouter.HandleFunc("/loaderio-e8b5a8357a3427f049705ec89394e548.txt", homecontroller.HomeLoaderIo).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
